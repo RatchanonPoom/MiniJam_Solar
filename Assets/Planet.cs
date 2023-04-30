7670b6af162391abe93ce2c570c20a467445ced9
ratchanon-p <rchn.ppsp@gmail.com>
2023-05-01 00:56:31 +0700

    (planet): Added planet gravity and rotation, new higher fidelity sphere mesh

--- Assets/Planet.cs
+++ Assets/Planet.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlanetGravity : MonoBehaviour
+public class Planet : MonoBehaviour
 {
     [SerializeField] float gravity;
+    [SerializeField] Vector3 rotateAxis = Vector3.up;
+    [SerializeField] float dayLength;
 
     Rigidbody[] affectedBodies;
 
@@ -15,11 +17,20 @@ public class PlanetGravity : MonoBehaviour
 
     private void FixedUpdate()
     {
+        transform.Rotate(rotateAxis, 2 * Mathf.PI / dayLength);
+
         foreach(Rigidbody body in affectedBodies)
         {
             Vector3 direction = body.transform.position - this.transform.position;
 
             body.AddForce(body.mass * gravity * direction * Time.fixedDeltaTime);
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        float axisDrawLength = 100;
+        Gizmos.DrawLine(this.transform.position + rotateAxis.normalized * axisDrawLength * .5f, this.transform.position - rotateAxis.normalized * axisDrawLength * .5f);
     }
 }
